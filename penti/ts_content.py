09d2877b9bf0695deceb4ff379d24143610db4cf
suddenlydream <suddenlydream@163.com>
2013-07-09 22:33:06 +0800

    add content

--- penti/ts_content.py
+++ penti/ts_content.py
@@ -5,8 +5,32 @@ Created on 2013-7-8
 '''
 
 from urllib.request import urlopen
+import re
+'''
+图说内容
+'''
 class ts_content:
+    '''
+        图说的段落
+    '''
     parts = []
+
+'''
+图说段落
+'''
+class ts_content_part:
+    '''
+        图说段落标题
+    '''
+    title = ''
+    '''
+        段落的子项
+    '''
+    items = []
+
+class ts_content_part_item:
+    txtInfo = ''
+    ref_url = '' 
     
 def parse(url):
     print(url)
@@ -18,4 +42,30 @@ def parse(url):
 def perform_parse(html):
     print(html)
     html = html.decode('GBK')
+#     p = re.compile(r'<P>【\d】.*?</P>')
+#     list = p.findall(html)
+#     list = re.findall(r'<P>【\d】.*?</P>', html)
+    li = re.finditer(r'<P>【\d*】.*?</P>', html)
+    i = 0
+
+    index = []
+    for m in li:
+        pos = m.start()
+        index.append(pos)
+        
+        if(i > 0):
+            start = int(index[i-1])
+            pos = int(pos)
+            print(start)
+            print(pos)
+            part = html[start, pos]
+            print(part)
+#         print(m.start())
+#         print(m.group())
+        i += 1
     return ''
+
+def parse_part(html):
+    print()
+
+parse('http://www.dapenti.com/blog/more.asp?name=xilei&id=79405')
